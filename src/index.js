f740f0163fc4afbcb13d32fadb626e97a7448828
Sergey Kozlov <sergeykozlov.2011@yandex.ru>
2017-12-02 00:10:19 +0300

    connect router

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import reducers from './reducers';
+import createBrowserHistory from 'history/createBrowserHistory';
+import {BrowserRouter,Route} from 'react-router-dom';
 
+import reducers from './reducers';
 import App from './components/App';
+import SignIn from './components/SignIn';
+import SignOut from './components/SignOut';
+
+const history=createBrowserHistory();
 
 
 const store=createStore(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
-        <MuiThemeProvider>
-            <App />
-        </MuiThemeProvider>
+            <MuiThemeProvider>
+                <BrowserRouter history={history}>
+                    <div>
+                        <Route exact path={'/'} component={App} />
+                        <Route path={'/signin'} component={SignIn} />
+                        <Route path={'/signout'} component={SignOut} />
+                    </div>
+                </BrowserRouter>
+            </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
 );
